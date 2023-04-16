6f0587dca174fcfdd2b99ae0c34d152870962164
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-04-16 03:30:39 -0700

    add results template

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 
 # from django.template import loader
@@ -32,9 +34,26 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request, question_id):
-    pass
-
-
 def vote(request, question_id):
-    pass
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request, 'polls/detail.html',
+            {
+                'question': question,
+                'error_message': 'You did not select a choice.'
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=question.id))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
