444d810a898164f3f17773568a25bd201f5bff45
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-04-15 22:04:41 -0700

    adding path separator for polls urls

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
-from django.template import loader
 from .models import Question
+
+
+# from django.template import loader
 
 
 # Create your views here.
@@ -20,12 +22,16 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}")
+    pass
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    pass
