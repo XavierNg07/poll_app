2eed22b1e4f4533549e22053da8b8955617a1ba8
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-04-25 10:11:33 -0700

    edit typo causes test fail

--- polls/tests.py
+++ polls/tests.py
@@ -84,7 +84,7 @@ class QuestionIndexViewTests(TestCase):
         """
         The questions index page may display multiple questions.
         """
-        question1 = create_question(question_text='Past question 1.', days=30)
+        question1 = create_question(question_text='Past question 1.', days=-30)
         question2 = create_question(question_text='Past question 2.', days=-5)
         response = self.client.get(reverse('polls:index'))
-        self.assertQuerySetEqual(response.context['latest_question_list'], [question1, question2])
+        self.assertQuerySetEqual(response.context['latest_question_list'], [question2, question1])
